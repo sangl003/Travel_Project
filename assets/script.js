4d2fd0b1249a951bca33364977c99fe33939c783
Git sangl003 <sharvari.sangle@gmail.com>
2018-08-23 19:59:38 -0500

    Geoloaction and places

--- assets/script.js
+++ assets/script.js
@@ -145,65 +145,113 @@ renderDropDownMenu();
 prompted by your browser. If you see the error "The Geolocation service
 failed.", it means you probably did not give permission for the browser to
 locate you.  */
-var map, infoWindow;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 6
-  });
-  infoWindow = new google.maps.InfoWindow;
 
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var service = new google.maps.places.PlacesService(map);
+// This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+    
+      var map;
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
-      infoWindow.open(map);
-      map.setCenter(pos);        
-      console.log(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, map.getCenter());
+      function initMap() {
+        // Create the map.
+        var pyrmont = {lat:44.970, lng: -93.244};
       
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+        $('#Restaurant')
+        $('#Shopping')
+        $('#')
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pyrmont,
+          zoom: 17
+        });
+        infoWindow = new google.maps.InfoWindow;
+        var service = new google.maps.places.PlacesService(map);
+        var getNextPage = null;
 
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+          // Try HTML5 geolocation.
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
+             // var service = new google.maps.places.PlacesService(map);
+
+              infoWindow.setPosition(pos);
+              infoWindow.setContent('Location found.');
+              infoWindow.open(map);
+              map.setCenter(pos);        
+              console.log(pos);
+            
+            service.nearbySearch(     
+            {
+              location: pos, radius: 500, type: ['store']
+            },
+              function(results, status, pagination) {
+              if (status !== 'OK') return;
+
+              createMarkers(results);
+              // moreButton.disabled = !pagination.hasNextPage;
+              // getNextPage = pagination.hasNextPage && function() {
+              //   pagination.nextPage();
+              // };
+            });
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+              
+            });
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
           }
+
+        // Create the places service.
+        
+        // var moreButton = document.getElementById('more');
+        // moreButton.onclick = function() {
+        //   moreButton.disabled = true;
+        //   if (getNextPage) getNextPage();
+        // };
+
+        // Perform a nearby search.
+      
+      }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      } // End of handleLocationError function
+
+
+      function createMarkers(places) {
+        var bounds = new google.maps.LatLngBounds();
+        var placesList = document.getElementById('places');
+
+
+
+        for (var i = 0, place; place = places[i]; i++) {
+          var image = {
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(25, 25)
+          };
+
+          var marker = new google.maps.Marker({
+            map: map,
+            icon: image,
+            title: place.name,
+            position: place.geometry.location
+          });
+
+          // var li = document.createElement('li');
+          // li.textContent = place.name;
+          // placesList.appendChild(li);
+
+          bounds.extend(place.geometry.location);
         }
-      } // End of callback function
-
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-  } // End of createMarker function
-
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-  } // End of handleLocationError function
-}
-
-// Create the places service.
+        map.fitBounds(bounds);
+      }
